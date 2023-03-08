aad3e7a4a4c01fe05aa25267385e8c8c9263689c
omatheu <124204749+omatheu@users.noreply.github.com>
2023-03-08 13:17:40 -0300

    Atualização da tela inicial

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,34 +3,33 @@ extends KinematicBody2D
 var velocidade : Vector2 = Vector2()
 var direction : Vector2 = Vector2()
 
-var motion = Vector2()
-
 func _ready():
-	$AnimatedSprite.flip_h = false
-	$AnimatedSprite.flip_v = false
+	pass
+	
 func read_input():
 	velocidade = Vector2()
 	
 	if Input.is_action_pressed("up"):
 		velocidade.y -= 1
 		direction = Vector2(0,-1)
-		$AnimatedSprite.flip_v = true
 		
-	if Input.is_action_pressed("down"):
+	elif Input.is_action_pressed("down"):
 		velocidade.y += 1
 		direction = Vector2(0,1)
-		$AnimatedSprite.flip_v = false
-		
-	if Input.is_action_pressed("left"):
+
+	elif Input.is_action_pressed("left"):
 		velocidade.x -= 1
 		direction = Vector2(-1,0)
-		$AnimatedSprite.flip_h = false
-		
-	if Input.is_action_pressed("rigth"):
+		$AnimationPlayer.play("Andando Direita") 
+
+	elif Input.is_action_pressed("rigth"):
 		velocidade.x += 1
 		direction = Vector2(1,0)
-		$AnimatedSprite.flip_h = true
-		
+		$AnimationPlayer.play("Andando Direita")
+	
+	else:
+		$AnimationPlayer.stop()
+
 	velocidade = velocidade.normalized()
 	velocidade = move_and_slide(velocidade * 200)
 	
